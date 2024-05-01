376a275b3e9136c7c081c8287bd1fef7e1db5d12
Matthew <matthew@quiqlabs.com>
2024-05-01 08:15:02 -0700

    new deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/patino-leaderboard'
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 	}
 };
